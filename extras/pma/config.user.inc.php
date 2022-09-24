5c6e0e19689907d96cddb1c281d5b88d31292641
Radek Kadner <radek.kadner@gmail.com>
2022-09-24 04:02:46 +0200

    .

--- extras/pma/config.user.inc.php
+++ extras/pma/config.user.inc.php
@@ -9,12 +9,17 @@
 
 declare(strict_types=1);
 
+# $cfg['blowfish_secret'] = '65145d665b8e660d25ac20bf30912ab3'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+
 /**
  * First server
  */
 $i++;
 /* Authentication type */
 /* Server parameters */
-$cfg['Servers'][$i]['ssl'] = true;
-$cfg['Servers'][$i]['ssl_ca'] = '/etc/phpmyadmin/cacert.pem';
+$cfg['Servers'][$i]['ssl'] = false;
+# $cfg['Servers'][$i]['ssl'] = true;
+# $cfg['Servers'][$i]['ssl_ca'] = '/etc/phpmyadmin/cacert.pem';
+# $cfg['Servers'][$i]['ssl_cert'] = '/etc/phpmyadmin/cert.pem';
+# $cfg['Servers'][$i]['ssl_key'] = '/etc/phpmyadmin/cert-key.pem';
 # $cfg['Servers'][$i]['ssl_verify'] = false;
